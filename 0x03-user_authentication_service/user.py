88c683ee54098bc657f1a4968bb4e440887ee1c4
ScottShadow <rudaseswascottmc@gmail.com>
2024-08-14 11:29:04 +0200

    added doc string 1

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""User Model Module"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
@@ -7,6 +8,7 @@ Base = declarative_base()
 
 
 class User(Base):
+    """User Representational model"""
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     email = Column(String(250), nullable=False)
