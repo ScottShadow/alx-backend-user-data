67ab794aa65321f3402c14fce605f1298917bebc
ScottShadow <rudaseswascottmc@gmail.com>
2024-08-16 11:13:33 +0200

    pycodestyle

--- 0x03-user_authentication_service/user-n.py
+++ 0x03-user_authentication_service/user-n.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
-"""User Model Module"""
-from sqlalchemy.ext.declarative import declarative_base
+"""User Model
+"""
 from sqlalchemy import Column, Integer, String
-
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class User(Base):
-    """User Representational model"""
-    __tablename__ = "users"
+    """User Model database table
+    """
+    __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
