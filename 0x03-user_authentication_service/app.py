7abc37b8a600914061f30ada6eb3f0cb36b0ea07
ScottShadow <rudaseswascottmc@gmail.com>
2024-08-16 18:01:11 +0200

    idk 2

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ User authentication service"""
-from flask import Flask, jsonify, request, Response, abort, redirect
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
